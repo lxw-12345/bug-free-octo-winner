60a06e5afc8fae41148a2859d6d3ea353f52cf5f
lxw-12345 <143623024+lxw-12345@users.noreply.github.com>
2023-09-25 23:42:53 +0800

    Add files via upload

---
+++ 九九乘法表/九九乘法表/九九乘法表.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int main()
+{
+	int i, j;
+	for (i = 1; i <= 9; i++)
+	{
+		for (j = 1; j <= i; j++)//注意此为j<=i，不是j<=9
+		{
+
+			printf("%d*%d=%d ", i, j, i * j);
+
+		}
+
+		printf("\n");//注意此句的位置
+	}
+
+	return 0;
+}
